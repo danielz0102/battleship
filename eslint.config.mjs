c80b28793ec31d188ad5ad5da4d075fec6db64fa
Daniel Zambrano <danyzglez@hotmail.com>
2025-02-06 21:04:26 -0600

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  pluginJs.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    overrides: [
+      {
+        files: ['tests/**/*'],
+        env: {
+          jest: true,
+        },
+      },
+    ],
+  },
+]
